9bb4b2f36f69b82971fa2badccc260d15fcbae4a
baekseungchan1117 <bsc7386@gmail.com>
2024-01-22 22:02:24 +0900

    회원 등록

--- ex-1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex-1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class JpaMain {
@@ -9,10 +10,23 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         EntityManager em = emf.createEntityManager();
-        //code
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        // JPA에 일어나는 변경은 트랜잭션 안에서 일어나야 한다.
+
+        try{
+            //code
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
-
-
     }
 }
